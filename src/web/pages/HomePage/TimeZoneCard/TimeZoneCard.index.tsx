b31d3b385018d57d9a51813468b5751c37461791
Thiago Spart <user000.000resu@gmail.com>
2022-08-29 15:43:39 -0300

    Add fahrenheit version

--- src/web/pages/HomePage/TimeZoneCard/TimeZoneCard.index.tsx
+++ src/web/pages/HomePage/TimeZoneCard/TimeZoneCard.index.tsx
@@ -2,14 +2,15 @@ import { FC } from "react"
 import * as S from "./TimeZoneCard.styled"
 import { TimeZoneCardProps } from "./TimeZoneCard.types"
 
-export const TimeZoneCard: FC<TimeZoneCardProps> = ({ predicatedStatusIconUrl, predicatedTemperature, timeZoneType, predicatedStatusIconAlt }) => {
+export const TimeZoneCard: FC<TimeZoneCardProps> = ({ temperatureVersion, predicatedStatusIconUrl, predicatedTemperature, timeZoneType, predicatedStatusIconAlt }) => {
 	return (
 		<S.Container>
 			<h5>{timeZoneType}</h5>
 			<div>
 				<img src={predicatedStatusIconUrl} alt={predicatedStatusIconAlt} />
 			</div>
-			<span aria-details="predicated temperature">{predicatedTemperature} °C</span>
+			<span aria-details="predicated temperature">
+				{predicatedTemperature} {temperatureVersion === "celsius" ? "C" : "F"}°</span>
 		</S.Container>
 	)
 }
